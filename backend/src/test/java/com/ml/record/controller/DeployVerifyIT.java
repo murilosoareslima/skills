d69147244fdf5fa3e8e38322b6482a71509ec43a
Murilo <murilosoareslima@gmail.com>
2022-04-08 15:26:40 -0300

    Refatorando test de deploy, deixando-o mais dinamico e excluindo imports nao utilizados]

--- backend/src/test/java/com/ml/record/controller/DeployVerifyIT.java
+++ backend/src/test/java/com/ml/record/controller/DeployVerifyIT.java
@@ -3,19 +3,38 @@ package com.ml.record.controller;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class DeployVerifyIT {
 
+    @Value("${server.host}")
+    private String serverHost;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Before
+    public void setup() {
+        RestAssured.baseURI = serverHost + ":" + serverPort;
+    }
+
     @Test
-    public void deployVerify() {
+    public void testDeployVerify() {
         String succssesReturn = "Api is running";
-        ValidatableResponse body = RestAssured.given().when().get("http://localhost:8080").then()        
+        ValidatableResponse body = RestAssured.given().when().get("/").then()        
         .assertThat().statusCode(200)
         .body(containsString(succssesReturn));
+        
         assertEquals(succssesReturn, body.extract().asString());
     }
 }
--- backend/src/test/java/com/ml/record/service/RecordServiceTest.java
+++ backend/src/test/java/com/ml/record/service/RecordServiceTest.java
@@ -2,7 +2,6 @@ package com.ml.record.service;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Optional;
